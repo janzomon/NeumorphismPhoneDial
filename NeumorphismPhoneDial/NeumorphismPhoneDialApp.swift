0247cc1236cd1330150c8f9c86e49280b8a5632b
Damon Gonzalez <damon.r.gonzalez@gmail.com>
2024-11-13 15:11:17 -0800

    Initial Commit

---
+++ NeumorphismPhoneDial/NeumorphismPhoneDialApp.swift
@@ -0,0 +1,17 @@
+//
+//  NeumorphismPhoneDialApp.swift
+//  NeumorphismPhoneDial
+//
+//  Created by Damon gonzalez on 11/13/24.
+//
+
+import SwiftUI
+
+@main
+struct NeumorphismPhoneDialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ NeumorphismPhoneDialTests/NeumorphismPhoneDialTests.swift
@@ -0,0 +1,17 @@
+//
+//  NeumorphismPhoneDialTests.swift
+//  NeumorphismPhoneDialTests
+//
+//  Created by Damon gonzalez on 11/13/24.
+//
+
+import Testing
+@testable import NeumorphismPhoneDial
+
+struct NeumorphismPhoneDialTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
