fd5037d38ad655579aafb2a5067bed55a7012e4d
Damon Gonzalez <damon.r.gonzalez@gmail.com>
2024-11-13 19:43:13 -0800

    updated 'struct RiggedSquareToSquareIndent: ButtonStyle' size and added () around first 3 numbers when 10 elements are added to array

--- NeumorphismPhoneDial/ButtonStyles.swift
+++ NeumorphismPhoneDial/ButtonStyles.swift
@@ -6,3 +6,267 @@
 //
 
 import Foundation
+import SwiftUI
+
+
+
+
+
+//MARK: - WHITE
+struct FlatStyle: ButtonStyle {
+    func makeBody(configuration: Self.Configuration) -> some View {
+        configuration.label
+        .padding(30)
+        .background(
+            Group {
+                if configuration.isPressed {
+                    Circle()
+                        .fill(Color.offWhite)
+                } else {
+                    Circle()
+                        .fill(Color.offWhite)
+                        .shadow(color: Color.black.opacity(0.2), radius: 10, x: 10, y: 10)
+                        .shadow(color: Color.white.opacity(0.7), radius: 10, x: -5, y: -5)
+                }
+            }
+        )
+    }
+}
+
+struct SoftIndent: ButtonStyle {
+    func makeBody(configuration: Self.Configuration) -> some View {
+        configuration.label
+        .padding(30)
+        .background(
+            Group {
+                if configuration.isPressed {
+                    Circle()
+                        .fill(Color.offWhite)
+                        .shadow(color: Color.black.opacity(0.2), radius: 10, x: -5, y: -5)
+                        .shadow(color: Color.white.opacity(0.7), radius: 10, x: 10, y: 10)
+                } else {
+                    Circle()
+                        .fill(Color.offWhite)
+                        .shadow(color: Color.black.opacity(0.2), radius: 10, x: 10, y: 10)
+                        .shadow(color: Color.white.opacity(0.7), radius: 10, x: -5, y: -5)
+                }
+            }
+        )
+    }
+}
+
+struct RiggedIndent: ButtonStyle {
+    func makeBody(configuration: Self.Configuration) -> some View {
+        configuration.label
+        .padding(30)
+        .background(
+            Group {
+                if configuration.isPressed {
+                    Circle()
+                    .fill(Color.offWhite)
+                    .overlay(
+                        Circle()
+                            .stroke(Color.gray, lineWidth: 4)
+                            .blur(radius: 4)
+                            .offset(x: 2, y: 2)
+                            .mask(Circle().fill(LinearGradient(Color.black, Color.clear)))
+                    )
+                    .overlay(
+                        Circle()
+                            .stroke(Color.white, lineWidth: 8)
+                            .blur(radius: 4)
+                            .offset(x: -2, y: -2)
+                            .mask(Circle().fill(LinearGradient(Color.clear, Color.black)))
+                    )
+                } else {
+                    Circle()
+                        .fill(Color.offWhite)
+                        .shadow(color: Color.black.opacity(0.2), radius: 10, x: 10, y: 10)
+                        .shadow(color: Color.white.opacity(0.7), radius: 10, x: -5, y: -5)
+                }
+            }
+        )
+    }
+}
+
+struct FlatSquareStyle: ButtonStyle {
+    func makeBody(configuration: Self.Configuration) -> some View {
+        configuration.label
+        .padding(30)
+        .background(
+            Group {
+                if configuration.isPressed {
+                    RoundedRectangle(cornerRadius: 10)
+                        .fill(Color.offWhite)
+                        .frame(width: 100, height: 50)
+                } else {
+                    RoundedRectangle(cornerRadius: 10)
+                        .fill(Color.offWhite)
+                        .frame(width: 100, height: 50)
+                        .shadow(color: Color.black.opacity(0.2), radius: 10, x: 10, y: 10)
+                        .shadow(color: Color.white.opacity(0.7), radius: 10, x: -5, y: -5)
+                }
+            }
+        )
+    }
+}
+
+struct SoftSquareIndent: ButtonStyle {
+    func makeBody(configuration: Self.Configuration) -> some View {
+        configuration.label
+        .padding(30)
+        .background(
+            Group {
+                if configuration.isPressed {
+                    RoundedRectangle(cornerRadius: 10)
+                        .fill(Color.offWhite)
+                        .frame(width: 100, height: 50)
+                        .shadow(color: Color.black.opacity(0.2), radius: 10, x: -5, y: -5)
+                        .shadow(color: Color.white.opacity(0.7), radius: 10, x: 10, y: 10)
+                } else {
+                    RoundedRectangle(cornerRadius: 10)
+                        .fill(Color.offWhite)
+                        .frame(width: 100, height: 50)
+                        .shadow(color: Color.black.opacity(0.2), radius: 10, x: 10, y: 10)
+                        .shadow(color: Color.white.opacity(0.7), radius: 10, x: -5, y: -5)
+                }
+            }
+        )
+    }
+}
+
+struct RiggedSquareIndent: ButtonStyle {
+    func makeBody(configuration: Self.Configuration) -> some View {
+        configuration.label
+        .padding(30)
+        .background(
+            Group {
+                if configuration.isPressed {
+                    RoundedRectangle(cornerRadius: 10)
+                    .fill(Color.offWhite)
+                    .frame(width: 100, height: 50)
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 10)
+                            .stroke(Color.gray, lineWidth: 4)
+                            .frame(width: 100, height: 50)
+                            .blur(radius: 4)
+                            .offset(x: 2, y: 2)
+                            .mask(RoundedRectangle(cornerRadius: 10).fill(LinearGradient(Color.black, Color.clear)))
+                    )
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 10)
+                            .stroke(Color.white, lineWidth: 8)
+                            .frame(width: 100, height: 50)
+                            .blur(radius: 4)
+                            .offset(x: -2, y: -2)
+                            .mask(RoundedRectangle(cornerRadius: 10).fill(LinearGradient(Color.clear, Color.black)))
+                    )
+                } else {
+                    RoundedRectangle(cornerRadius: 10)
+                        .fill(Color.offWhite)
+                        .frame(width: 100, height: 50)
+                        .shadow(color: Color.black.opacity(0.2), radius: 10, x: 10, y: 10)
+                        .shadow(color: Color.white.opacity(0.7), radius: 10, x: -5, y: -5)
+                }
+            }
+        )
+    }
+}
+
+struct FlatSquareToSquareStyle: ButtonStyle {
+    func makeBody(configuration: Self.Configuration) -> some View {
+        configuration.label
+        .padding(30)
+        .background(
+            Group {
+                if configuration.isPressed {
+                    RoundedRectangle(cornerRadius: 25)
+                        .fill(Color.offWhite)
+                        .frame(width: 70, height: 70)
+                } else {
+                    RoundedRectangle(cornerRadius: 10)
+                        .fill(Color.offWhite)
+                        .frame(width: 50, height: 50)
+                        .shadow(color: Color.black.opacity(0.2), radius: 10, x: 10, y: 10)
+                        .shadow(color: Color.white.opacity(0.7), radius: 10, x: -5, y: -5)
+                }
+            }
+        )
+    }
+}
+
+struct SoftSquareToSquareIndent: ButtonStyle {
+    func makeBody(configuration: Self.Configuration) -> some View {
+        configuration.label
+        .padding(30)
+        .background(
+            Group {
+                if configuration.isPressed {
+                    RoundedRectangle(cornerRadius: 20)
+                        .fill(Color.offWhite)
+                        .frame(width: 70, height: 70)
+                        .shadow(color: Color.black.opacity(0.2), radius: 10, x: -5, y: -5)
+                        .shadow(color: Color.white.opacity(0.7), radius: 10, x: 10, y: 10)
+                } else {
+                    RoundedRectangle(cornerRadius: 10)
+                        .fill(Color.offWhite)
+                        .frame(width: 50, height: 50)
+                        .shadow(color: Color.black.opacity(0.2), radius: 10, x: 10, y: 10)
+                        .shadow(color: Color.white.opacity(0.7), radius: 10, x: -5, y: -5)
+                }
+            }
+        )
+    }
+}
+
+struct RiggedSquareToSquareIndent: ButtonStyle {
+    func makeBody(configuration: Self.Configuration) -> some View {
+        configuration.label
+        .padding(30)
+        .background(
+            Group {
+                if configuration.isPressed {
+                    RoundedRectangle(cornerRadius: 20)
+                        .fill(Color.offWhite)
+                        .frame(width: 120, height: 120)
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 20)
+                            .stroke(Color.gray, lineWidth: 4)
+                            .frame(width: 120, height: 120)
+                            .blur(radius: 4)
+                            .offset(x: 2, y: 2)
+                            .mask(RoundedRectangle(cornerRadius: 20).fill(LinearGradient(Color.black, Color.clear)))
+                    )
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 20)
+                            .stroke(Color.white, lineWidth: 8)
+                            .frame(width: 120, height: 120)
+                            .blur(radius: 4)
+                            .offset(x: -2, y: -2)
+                            .mask(RoundedRectangle(cornerRadius: 20).fill(LinearGradient(Color.clear, Color.black)))
+                    )
+                } else {
+                    RoundedRectangle(cornerRadius: 10)
+                        .fill(Color.offWhite)
+                        .frame(width: 100, height: 100)
+                        .shadow(color: Color.black.opacity(0.2), radius: 10, x: 10, y: 10)
+                        .shadow(color: Color.white.opacity(0.7), radius: 10, x: -5, y: -5)
+                }
+            }
+        )
+    }
+}
+
+
+
+//EXTENSIONS
+extension Color {
+    static let offWhite = Color(red: 225 / 255, green: 225 / 255, blue: 235 / 255)
+}
+
+
+extension LinearGradient {
+    init(_ colors: Color...) {
+        self.init(gradient: Gradient(colors: colors), startPoint: .topLeading, endPoint: .bottomTrailing)
+    }
+}
--- NeumorphismPhoneDial/ContentView.swift
+++ NeumorphismPhoneDial/ContentView.swift
@@ -6,16 +6,258 @@
 //
 
 import SwiftUI
+import AVFoundation
 
 struct ContentView: View {
+    @State var dialedNumbers: [String] = []
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            //BACKGROUND
+            Color.offWhite
+            VStack {
+                //NUMBER DISPLAY STACK
+                HStack(alignment: .center,spacing: 10) {
+                    switch dialedNumbers.count {
+                    case 10:
+                        Text("+1 " + "(\(dialedNumbers[0...2].joined()))" + "-\(dialedNumbers[3...5].joined())" + "-\(dialedNumbers[6...9].joined())")
+                            .font(.system(size: 30))
+                            .fontWeight(.heavy)
+                            .foregroundStyle(.gray)
+                    case 11:
+                        Text("\(dialedNumbers.joined())")
+                            .font(.system(size: 30))
+                            .fontWeight(.heavy)
+                            .foregroundStyle(.gray)
+                    default:
+                        Text("\(dialedNumbers.joined())")
+                            .font(.system(size: 30))
+                            .fontWeight(.heavy)
+                            .foregroundStyle(.gray)
+                    }
+                }
+                .frame(width: 320, height: 80)
+                //.border(.red)
+                .padding(.bottom,10)
+                //NUMBER PAD STACK
+                VStack {
+                    HStack(alignment: .top, spacing: 40) {
+                        //1,4,7,*
+                        VStack(spacing: 40) {
+                            //1
+                            Button(action: {
+                                if dialedNumbers.count < 10 {
+                                    dialedNumbers.append("1")
+                                    AudioServicesPlaySystemSound(1306)
+                                }
+                                else {
+                                    AudioServicesPlaySystemSound(1306)
+                                }
+                            }) {
+                                Text("1")
+                                    .font(.headline)
+                                    .fontWeight(.heavy)
+                                    .foregroundColor(.gray)
+                            }
+                            .buttonStyle(RiggedSquareToSquareIndent())
+                            //4
+                            Button(action: {
+                                if dialedNumbers.count < 10 {
+                                    dialedNumbers.append("4")
+                                    AudioServicesPlaySystemSound(1306)
+                                }
+                                else {
+                                    AudioServicesPlaySystemSound(1306)
+                                }
+                            }) {
+                                Text("4")
+                                    .font(.headline)
+                                    .fontWeight(.heavy)
+                                    .foregroundColor(.gray)
+                            }
+                            .buttonStyle(RiggedSquareToSquareIndent())
+                            //7
+                            Button(action: {
+                                if dialedNumbers.count < 10 {
+                                    dialedNumbers.append("7")
+                                    AudioServicesPlaySystemSound(1306)
+                                }
+                                else {
+                                    AudioServicesPlaySystemSound(1306)
+                                }
+                            }) {
+                                Text("7")
+                                    .font(.headline)
+                                    .fontWeight(.heavy)
+                                    .foregroundColor(.gray)
+                            }
+                            .buttonStyle(RiggedSquareToSquareIndent())
+                            //*
+                            Button(action: {
+                                if dialedNumbers.count < 10 {
+                                    AudioServicesPlaySystemSound(1306)
+                                }
+                                else {
+                                    AudioServicesPlaySystemSound(1306)
+                                }
+                            }) {
+                                Text("*")
+                                    .font(.headline)
+                                    .fontWeight(.heavy)
+                                    .foregroundColor(.gray)
+                            }
+                            .buttonStyle(RiggedSquareToSquareIndent())
+                        }
+                        //2,5,8,0,phone
+                        VStack(spacing: 40) {
+                            //2
+                            Button(action: {
+                                if dialedNumbers.count < 10 {
+                                    dialedNumbers.append("2")
+                                    AudioServicesPlaySystemSound(1306)
+                                }
+                                else {
+                                    AudioServicesPlaySystemSound(1306)
+                                }
+                            }) {
+                                Text("2")
+                                    .font(.headline)
+                                    .fontWeight(.heavy)
+                                    .foregroundColor(.gray)
+                            }
+                            .buttonStyle(RiggedSquareToSquareIndent())
+                            //5
+                            Button(action: {
+                                if dialedNumbers.count < 10 {
+                                    dialedNumbers.append("5")
+                                    AudioServicesPlaySystemSound(1306)
+                                }
+                                else {
+                                    AudioServicesPlaySystemSound(1306)
+                                }
+                            }) {
+                                Text("5")
+                                    .font(.headline)
+                                    .fontWeight(.heavy)
+                                    .foregroundColor(.gray)
+                            }
+                            .buttonStyle(RiggedSquareToSquareIndent())
+                            //8
+                            Button(action: {
+                                if dialedNumbers.count < 10 {
+                                    dialedNumbers.append("8")
+                                    AudioServicesPlaySystemSound(1306)
+                                }
+                                else {
+                                    AudioServicesPlaySystemSound(1306)
+                                }
+                            }) {
+                                Text("8")
+                                    .font(.headline)
+                                    .fontWeight(.heavy)
+                                    .foregroundColor(.gray)
+                            }
+                            .buttonStyle(RiggedSquareToSquareIndent())
+                            //0
+                            Button(action: {
+                                if dialedNumbers.count < 10 {
+                                    dialedNumbers.append("0")
+                                    AudioServicesPlaySystemSound(1306)
+                                }
+                                else {
+                                    AudioServicesPlaySystemSound(1306)
+                                }
+                            }) {
+                                Text("0")
+                                    .font(.headline)
+                                    .fontWeight(.heavy)
+                                    .foregroundColor(.gray)
+                            }
+                            .buttonStyle(RiggedSquareToSquareIndent())
+                            Button(action: {
+                                AudioServicesPlaySystemSound(1306)
+                            }) {
+                                Image(systemName: "phone")
+                                    .foregroundStyle(.green)
+                            }
+                            .buttonStyle(RiggedSquareToSquareIndent())
+                        }
+                        //3,6,9,#,delete
+                        VStack(spacing: 40) {
+                            //3
+                            Button(action: {
+                                if dialedNumbers.count < 10 {
+                                    dialedNumbers.append("3")
+                                    AudioServicesPlaySystemSound(1306)
+                                }
+                                else {
+                                    AudioServicesPlaySystemSound(1306)
+                                }
+                            }) {
+                                Text("3")
+                                    .font(.headline)
+                                    .fontWeight(.heavy)
+                                    .foregroundColor(.gray)
+                            }
+                            .buttonStyle(RiggedSquareToSquareIndent())
+                            //6
+                            Button(action: {
+                                if dialedNumbers.count < 10 {
+                                    dialedNumbers.append("6")
+                                    AudioServicesPlaySystemSound(1306)
+                                }
+                                else {
+                                    AudioServicesPlaySystemSound(1306)
+                                }
+                            }) {
+                                Text("6")
+                                    .font(.headline)
+                                    .fontWeight(.heavy)
+                                    .foregroundColor(.gray)
+                            }
+                            .buttonStyle(RiggedSquareToSquareIndent())
+                            //9
+                            Button(action: {
+                                if dialedNumbers.count < 10 {
+                                    dialedNumbers.append("9")
+                                    AudioServicesPlaySystemSound(1306)
+                                }
+                                else {
+                                    AudioServicesPlaySystemSound(1306)
+                                }
+                            }) {
+                                Text("9")
+                                    .font(.headline)
+                                    .fontWeight(.heavy)
+                                    .foregroundColor(.gray)
+                            }
+                            .buttonStyle(RiggedSquareToSquareIndent())
+                            //#
+                            Button(action: {
+                                AudioServicesPlaySystemSound(1306)
+                            }) {
+                                Text("#")
+                            }
+                            .buttonStyle(RiggedSquareToSquareIndent())
+                            //delete
+                            Button(action: {
+                                if dialedNumbers.isEmpty == true {
+                                    AudioServicesPlaySystemSound(1306)
+                                }
+                                else {
+                                    dialedNumbers.removeLast()
+                                    AudioServicesPlaySystemSound(1306)
+                                }
+                            }) {
+                                Image(systemName: "delete.left")
+                                    .foregroundStyle(.red)
+                            }
+                            .buttonStyle(RiggedSquareToSquareIndent())
+                        }
+                    }
+                }
+            }
         }
-        .padding()
+        .edgesIgnoringSafeArea(.all)
     }
 }
 
